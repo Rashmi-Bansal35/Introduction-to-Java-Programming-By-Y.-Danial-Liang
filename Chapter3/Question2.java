0f9efc8799234e85abc00ebd101d62d1e9c7829d
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-08-26 19:18:06 +0530

    add three numbers

---
+++ Chapter3/Question2.java
@@ -0,0 +1,12 @@
+/*
+The program in Listing 3.1, AdditionQuiz.java, generates two integers and prompts the user to enter the sum of these two integers.
+Revise the program to generate three single-digit integers and prompt the user to enter the sum of these three integers.
+*/
+
+package Chapter3;
+
+public class Question2 {
+    public static void main(String[] args){
+
+    }
+}
