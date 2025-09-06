e08d009658fdd0ada351a0b68f10c537aab6238f
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-06 19:20:47 +0530

    add three numbers

--- Chapter3/Question2.java
+++ Chapter3/Question2.java
@@ -7,8 +7,23 @@ package Chapter3;
 import java.util.Scanner;
 
 public class Question2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // Create a Scanner
         Scanner input = new Scanner(System.in);
+
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+        int number3 = (int)(System.currentTimeMillis() / 8 % 10);
+
+        System.out.print("What is " + number1 + " + " + number2 + " + " + number3 + "? ");
+
+        int answer = input.nextInt();
+        if(number1+number2+number3 == answer){
+            System.out.println(number1 + " + " + number2 + " = " + answer + " is correct." );
+        }
+        else{
+            System.out.println(number1 + " + " + number2 + " = " + answer + " is incorrect." );
+        }
 
         input.close();
     }
