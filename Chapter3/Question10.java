ce3053f254386a8050bb81fdac50ccf99d6e3b9d
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-06 19:54:52 +0530

    Listing 3.3

---
+++ Chapter3/Question10.java
@@ -0,0 +1,14 @@
+/*
+Listing 3.3, SubtractionQuiz.java, randomly generates a subtraction question. Revise the program to randomly generate an addition question with two integers less than 100. 
+*/
+package Chapter3;
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        // Close the Scanner
+        input.close();
+    }
+}
