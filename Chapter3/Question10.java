adeb4193768568af78dfc667c244e5681286bbbb
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-06 21:57:58 +0530

    Addition quiz

--- Chapter3/Question10.java
+++ Chapter3/Question10.java
@@ -8,6 +8,23 @@ public class Question10 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
+        // 𝘎𝘦𝘯𝘦𝘳𝘢𝘵𝘦 𝘵𝘸𝘰 𝘳𝘢𝘯𝘥𝘰𝘮 𝘥𝘪𝘨𝘪𝘵 𝘯𝘶𝘮𝘣𝘦𝘳𝘴
+        int number1 = (int)(Math.random() * 100);
+        int number2 = (int)(Math.random() * 100);
+
+        System.out.print("What is " + number1 + " + " + number2 + "? ");
+
+        // Input the answer 
+        int answer = input.nextInt();
+
+        //⁡⁣⁢⁣ ‍‍Grade the answer and display the result
+        if(number1 + number2 == answer){
+            System.out.println("You are correct.");
+        }
+        else{
+            System.out.println("Your answer is wrong.");
+            System.out.println(number1 + "+" + number2 + " should be " + (number1 + number2));
+        }
         // Close the Scanner
         input.close();
     }
