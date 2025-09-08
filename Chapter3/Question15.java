60756524c1c6cb21455647f936c4bed0c626a920
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-08 22:13:45 +0530

    game lottery

--- Chapter3/Question15.java
+++ Chapter3/Question15.java
@@ -25,7 +25,7 @@ public class Question15 {
         int lotteryDigit3 = lottery % 10;
 
         // Get digits from guess
-        int guessDigit1 = guess / 10;
+        int guessDigit1 = guess / 100;
         int guessDigit2 = (guess % 100)/10;
         int guessDigit3 = guess % 10;
 
@@ -34,9 +34,9 @@ public class Question15 {
         // Check the guess
         if (guess == lottery)
             System.out.println("Exact match: you win $10,000");
-        else if (guessDigit2 == lotteryDigit1 && guessDigit1 == lotteryDigit2 && guessDigit3 == lotteryDigit3)
+        else if ((guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit1 == lotteryDigit3) &&(guessDigit2 == lotteryDigit1 || guessDigit2 == lotteryDigit2 || guessDigit2 == lotteryDigit3) &&(guessDigit3 == lotteryDigit1 || guessDigit3 == lotteryDigit2 || guessDigit3 == lotteryDigit3) &&(guessDigit1 != guessDigit2 || lotteryDigit1 != lotteryDigit2 || lotteryDigit1 != lotteryDigit3))
             System.out.println("Match all digits: you win $3,000");
-        else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit2 == lotteryDigit1|| guessDigit2 == lotteryDigit2 || guessDigit3 == lotteryDigit1 || guessDigit3 == lotteryDigit2)
+        else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit1 == lotteryDigit3 ||guessDigit2 == lotteryDigit1 || guessDigit2 == lotteryDigit2 || guessDigit2 == lotteryDigit3 || guessDigit3 == lotteryDigit1 || guessDigit3 == lotteryDigit2 || guessDigit3 == lotteryDigit3)
             System.out.println("Match one digit: you win $1,000");
 
         // Close the Scanner
