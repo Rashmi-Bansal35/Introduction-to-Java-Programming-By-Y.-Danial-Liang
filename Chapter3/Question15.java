775f49a33626a99e101b816c112ad2912a37dbe8
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-08 22:05:14 +0530

    game lottery

--- Chapter3/Question15.java
+++ Chapter3/Question15.java
@@ -12,6 +12,33 @@ public class Question15 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
+        // Generate a lottery number
+        int lottery = (int)(Math.random() * 1000);
+
+        // Prompt the user to enter a guess
+        System.out.print("Enter your lottery pick (three digits): ");
+        int guess = input.nextInt();
+
+        // Get digits from lottery
+        int lotteryDigit1 = lottery / 100;
+        int lotteryDigit2 = (lottery % 100)/10;
+        int lotteryDigit3 = lottery % 10;
+
+        // Get digits from guess
+        int guessDigit1 = guess / 10;
+        int guessDigit2 = (guess % 100)/10;
+        int guessDigit3 = guess % 10;
+
+        System.out.println("The lottery number is " + lottery);
+
+        // Check the guess
+        if (guess == lottery)
+            System.out.println("Exact match: you win $10,000");
+        else if (guessDigit2 == lotteryDigit1 && guessDigit1 == lotteryDigit2 && guessDigit3 == lotteryDigit3)
+            System.out.println("Match all digits: you win $3,000");
+        else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit2 == lotteryDigit1|| guessDigit2 == lotteryDigit2 || guessDigit3 == lotteryDigit1 || guessDigit3 == lotteryDigit2)
+            System.out.println("Match one digit: you win $1,000");
+
         // Close the Scanner
         input.close(); 
     }
