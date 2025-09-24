4d8c85a1eccbad55455c457e30875899718fa79d
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-09-24 18:00:47 +0530

    pick a card

---
+++ Chapter3/Question24.java
@@ -0,0 +1,11 @@
+/*
+Write a program that simulates picking a card from a deck of 52 cards. Your program should display the rank (Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King) and suit (Clubs, Diamonds, Hearts, Spades) of the card.
+*/
+
+package Chapter3;
+
+public class Question24 {
+    public static void main(String[] args){
+        
+    }
+}
