988153233ba93064c68b16ececad73ee655507d0
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-12-21 19:28:39 +0530

    Check SSN

--- Chapter4/Question21.java
+++ Chapter4/Question21.java
@@ -8,6 +8,33 @@ import java.util.Scanner;
 public class Question21 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        
+        // Prompt the user to enter a social security number
+        System.out.print("Enter a SSN: ");
+        String ssn = input.next();
+
+        // Check the length of input
+        if (ssn.length() != 11){
+            System.out.println(ssn + " is an invalid social security number.");
+        }
+        else {
+            // Check the place of dashes
+            boolean dash1 = (ssn.charAt(3) == '-');
+            boolean dash2 = (ssn.charAt(6) == '-');
+
+            // Check all other characters are digit
+            boolean firstPart = Character.isDigit(ssn.charAt(0)) && Character.isDigit(ssn.charAt(1)) && Character.isDigit(ssn.charAt(2));
+            boolean middlePart = Character.isDigit(ssn.charAt(4)) && Character.isDigit(ssn.charAt(5));
+            boolean lastPart = Character.isDigit(ssn.charAt(7)) && Character.isDigit(ssn.charAt(8)) && Character.isDigit(ssn.charAt(9)) &&  Character.isDigit(ssn.charAt(10));
+
+            // Check if input is valid or not and display output according to that
+            if (dash1 && dash2 && firstPart && middlePart && lastPart){
+                System.out.println(ssn + " is a valid social security number");
+            }
+            else {
+                System.out.println(ssn + " is an invalid social security number");
+            }
+        }
 
         // Close the Scanner
         input.close();
