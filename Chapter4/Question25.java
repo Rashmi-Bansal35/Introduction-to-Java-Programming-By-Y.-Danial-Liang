765eef1501315fa3a34411ef595d2bfd891f97ba
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2025-12-21 19:49:50 +0530

    Generate vehicle plate number

--- Chapter4/Question25.java
+++ Chapter4/Question25.java
@@ -1,17 +1,24 @@
 /*
 Assume a vehicle plate number consists of three uppercase letters followed by four digits. Write a program to generate a plate number.
+Like⁡⁢⁢⁣ ⁡⁣⁢⁣𝗟𝗟𝗟𝗗𝗗𝗗𝗗⁡ here, L is letter and D is digit
 */
 
 package Chapter4;
-import java.util.Scanner;
 
 public class Question25 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        // Generate random letters
+        char letter1 = (char) ('A' + (Math.random() * 26));
+        char letter2 = (char) ('A' + (Math.random() * 26));
+        char letter3 = (char) ('A' + (Math.random() * 26));
 
-        // Prompt the user to enter 
+        // Generate random digits
+        int digit1 = (int) (Math.random() * 10);
+        int digit2 = (int) (Math.random() * 10);
+        int digit3 = (int) (Math.random() * 10);
+        int digit4 = (int) (Math.random() * 10);
 
-        // Close the Scanner
-        input.close();
+        // Display the output
+        System.out.println("The generated plate number is " + letter1 + letter2 + letter3 + digit1 + digit2 + digit3 + digit4);
     }
 }
