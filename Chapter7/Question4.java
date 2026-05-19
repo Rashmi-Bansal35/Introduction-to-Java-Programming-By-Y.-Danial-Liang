ff7ec76b60646bbeb6eb0af7bfcf05d71ab8a31b
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2026-05-19 22:55:31 +0530

    Add program to read scores and count students above and below average

--- Chapter7/Question4.java
+++ Chapter7/Question4.java
@@ -8,6 +8,32 @@ import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int belowAvg = 0, aboveAvg = 0, total = 0, sum = 0;
+        int[] scores = new int[100];
+        System.out.print("Enter scores: ");
+        while(true){
+            int number = input.nextInt();
+            if (number < 0 || total == 100){
+                break;
+            }
+            else{
+                scores[total] = number;
+                sum += scores[total];
+                total++;
+            }
+        }
+
+        double avg = (double) sum / total;
+        for (int i = 0; i < total; i++){
+            if (scores[i] < avg){
+                belowAvg++;
+            }
+            else{
+                aboveAvg++;
+            }
+        }
+
+        System.out.println("Number of below average students: " +belowAvg + ". \nNumber of above average students: " +aboveAvg);
 
         input.close();
     }
