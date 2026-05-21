36fb377ef211b85f3be91dc83088b944fb9b88fa
Rashmi-Bansal35 <gupta126idraghav@gmail.com>
2026-05-21 23:42:33 +0530

    Add programs to display the first 50 prime numbers and calculate the average of an array

--- Chapter7/Question6.java
+++ Chapter7/Question6.java
@@ -1,10 +1,37 @@
 /*
-Listing 5.15 determines whether a number n is prime by checking whether 2, 3, 4, 5, 6, ..., n/2 is a divisor. If a divisor is found, n is not prime. A more efficient approach is to check whether any of the prime number less than or equal to √n can divide n evenly. If not, n is prime. Rewrite Listing 5.15 to display the first 50 prime numbers using this approach. You need to use an array to store the prime numbers and later use them to check whether they are possible divisors for n.
+Listing 5.15 PrimeNumber.java determines whether a number n is prime by checking whether 2, 3, 4, 5, 6, ..., n/2 is a divisor. If a divisor is found, n is not prime. A more efficient approach is to check whether any of the prime number less than or equal to √n can divide n evenly. If not, n is prime. Rewrite Listing 5.15 to display the first 50 prime numbers using this approach. You need to use an array to store the prime numbers and later use them to check whether they are possible divisors for n.
 */
 package Chapter7;
 
 public class Question6 {
     public static void main(String[] args){
+        final int NUMBER_OF_PRIMES = 50; 
+        final int NUMBER_OF_PRIMES_PER_LINE = 10; 
+        int count = 0; 
+        int number = 2; 
+        int[] primeNumbers = new int[NUMBER_OF_PRIMES];
 
+        System.out.println("The first 50 prime numbers are \n");
+        while (count < NUMBER_OF_PRIMES) {
+            boolean isPrime = true; 
+
+            for (int i = 0; i < count && primeNumbers[i] <= Math.sqrt(number); i++) {
+                if (number % primeNumbers[i] == 0) { 
+                    isPrime = false; 
+                    break; 
+                }
+            }
+            if (isPrime) {
+                primeNumbers[count] = number; 
+                count++;
+
+                if (count % NUMBER_OF_PRIMES_PER_LINE == 0) {
+                    System.out.println(number);
+                } else {
+                    System.out.print(number + " ");
+                }
+            }
+            number++;
+        }
     }
 }
--- Chapter7/Question8.java
+++ Chapter7/Question8.java
@@ -9,17 +9,34 @@ import java.util.Scanner;
 
 public class Question8 {
     public static int average(int[] array){
-
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        int average = sum / array.length;
+        return average;
     }
 
     public static double average(double[] array){
-
+        double sum = 0;
+        for (int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        double average = sum / array.length;
+        return average;
     }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
+        double[] array = new double[10];
+        System.out.print("Enter ten values: ");
+        for(int i = 0; i < array.length; i++){
+            array[i] = input.nextDouble();
+        }
 
+        System.out.println("The average is " + String.format("%.2f", average(array)));
 
+        input.close();
     }
 }
